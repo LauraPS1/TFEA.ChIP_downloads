e1a77d9262e65cc00acb91674a2c4a4691278919
Laura <laura.psantamaria@gmail.com>
2017-09-06 09:56:23 +0200

    Update All-functions.R

--- All-functions.R
+++ All-functions.R
@@ -686,19 +686,19 @@ plot_CM<-function(CM.statMatrix,plot_title = NULL,specialTF = NULL,TF_colors = N
     
     if(length(CM.statMatrix[CM.statMatrix$OR==Inf,1])>0){
         warn<-length(CM.statMatrix[CM.statMatrix$OR==Inf,1])
-        CM.statMatrix[CM.statMatrix$OR==Inf,]$OR<-rep(max(CM.statMatrix[CM.statMatrix$OR!=Inf]$OR),
+        CM.statMatrix[CM.statMatrix$OR==Inf,]$OR<-rep(max(CM.statMatrix[CM.statMatrix$OR!=Inf,]$OR),
                                                       length(CM.statMatrix[CM.statMatrix$OR==Inf,1]))
         warning(warn," elements have an Odds Ratio of Inf. Maximum value for OR introduced instead.")
     }
     if(length(CM.statMatrix[CM.statMatrix$OR==-Inf,1])>0){
         warn<-length(CM.statMatrix[CM.statMatrix$OR==-Inf,1])
-        CM.statMatrix[CM.statMatrix$OR==-Inf,]$OR<-rep(min(CM.statMatrix[CM.statMatrix$OR!=-Inf]$OR),
+        CM.statMatrix[CM.statMatrix$OR==-Inf,]$OR<-rep(min(CM.statMatrix[CM.statMatrix$OR!=-Inf,]$OR),
                                                        length(CM.statMatrix[CM.statMatrix$OR==-Inf,1]))
         warning(warn," elements have an Odds Ratio of -Inf. Minimum value for OR introduced instead.")
     }
     if(length(CM.statMatrix[CM.statMatrix$p.value==0,1])>0){
         warn<-length(CM.statMatrix[CM.statMatrix$p.value==0,1])
-        CM.statMatrix[CM.statMatrix$p.value==0,]$adj.p.value<-rep(max(CM.statMatrix[CM.statMatrix$p.value!=0]$log.adj.pVal),
+        CM.statMatrix[CM.statMatrix$p.value==0,]$adj.p.value<-rep(max(CM.statMatrix[CM.statMatrix$p.value!=0,]$log.adj.pVal),
                                                                   length(CM.statMatrix[CM.statMatrix$p.value==0,1]))
         warning(warn," elements have a -log(p-Value) of Inf. Maximum value for -log(p-Val) introduced instead.")
     }
